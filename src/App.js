fb22c86cd3bd5194600cbee72e564ae72c2b8273
Saurav <you@example.comjsrana@LAPTOP-O615J2RF.(none)>
2020-12-13 16:57:15 +0530

    initial commit completed MyNavbar component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Components
+import MyNavbar from "./components/my-navbar/mynavbar.components";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar />
+      My Portfolio
+    </div>
+  );
+};
+
+export default App;
