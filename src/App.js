644da63b3b31c5068fb9fc4a78c06fdb96fd274e
Saurav <you@example.comjsrana@LAPTOP-O615J2RF.(none)>
2020-12-16 19:59:00 +0530

    Skills page added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MyNavbar from "./components/my-navbar/mynavbar.components";
 import MyCarousal from "./components/my-carousal/mycarousal.components";
 import TitleMessage from "./components/title-message/title-message.components";
 import About from "./pages/about/about.component";
+import Skills from "./pages/skills/skills.component";
 
 import "./App.css";
 
@@ -19,13 +20,29 @@ const App = () => {
       <TitleMessage />
       {/* About me section */}
       <div>
-        <Parallax bgImage="./assets/img/parallex/download.jpg" strength={100}>
-          <Container className="container-box rounded">
-            <Fade duration={500}>
-              <About />
-            </Fade>
-          </Container>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+        >
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
         </Parallax>
+        {/* skills section */}
+      </div>
+      <div>
+        <Container className="container-box rounded">
+          <Fade duration={500}>
+            <hr />
+            <Skills />
+          </Fade>
+        </Container>
       </div>
     </div>
   );
